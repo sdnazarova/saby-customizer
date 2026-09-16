9c3d3a3b3258127b3f621bcc7bdb05f9be463de3
cursed <cursedbeing@vk.com>
2026-09-16 12:55:01 +0300

    Осенняя тема

--- README.md
+++ README.md
@@ -10,3 +10,22 @@
 «Лучший день — сегодня» [установить](https://github.com/sdnazarova/saby-customizer/raw/main/autumn_2026/SABY-Autumn-lotus-button.user.js)
 
 <img src="autumn_2026/src-img/lotus_button.svg" alt="Lotus" width="250">
+
+
+#### Осенняя тема 
+
+Иннокентий в листьях [установить](https://github.com/sdnazarova/saby-customizer/raw/main/autumn_2026/SABY-Autumn-Innokentiy-leafs-static.user.js)
+
+<img src="autumn_2026/src-img/Innokentiy_leafs.png" alt="Lotus" width="250">
+
+Иннокентий c чаем [установить](https://github.com/sdnazarova/saby-customizer/raw/main/autumn_2026/SABY-Autumn-Innokentiy-tea-static.user.js)
+
+<img src="autumn_2026/src-img/Innokentiy_tea.png" alt="Lotus" width="250">
+
+Иннокентий c зонтиком [установить](https://github.com/sdnazarova/saby-customizer/raw/main/autumn_2026/SABY-Autumn-Innokentiy-umbrella-static.user.js)
+
+<img src="autumn_2026/src-img/Innokentiy_umbrella.png" alt="Lotus" width="250">
+
+Иннокентий преподает [установить](https://github.com/sdnazarova/saby-customizer/raw/main/autumn_2026/SABY-Autumn-Innokentiy-teacher-static.user.js)
+
+<img src="autumn_2026/src-img/Innokentiy_teacher.png" alt="Lotus" width="250">
---
+++ autumn_2026/SABY-Autumn-Innokentiy-leafs-static.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name          SABY Innokentiy Leafs
+// @namespace     saby-customizer
+// @version       1.0.1
+// @author        Nazarova Svetlana sd.nazarova@tensor.ru
+// @description   Персональная настройка saby приложений для решения повседневных задач, и не только...
+// @include       https://online.sbis.ru/*
+// @include       https://online.saby.ru/*
+// @include       https://new-edu.saby.ru/*
+// @run-at        document-end
+// @grant         unsafeWindow
+// @noframes
+// ==/UserScript==
+/* global unsafeWindow */
+(({ document }) => {
+  const style = document.createElement('style')
+
+  style.type = 'text/css'
+  style.innerHTML = `
+    .NavigationPanels-Accordion__title_level-1::after, .NavigationPanels-Accordion__title_level-2_active::after {
+      background: none !important;
+    }
+    .cloud-holiday_new-year .NavigationPanels-Accordion__container::before {
+      background: none !important;
+    }
+    .NavigationPanels-Accordion__tree {
+      background: none !important;
+    }
+
+    .NavigationPanels-Sidebar {
+      background-image: url(https://new-edu.saby.ru/tensor/new_year/2026/autumn_theme/Innokentiy_tea.png) !important;
+      background-repeat: no-repeat !important;
+      background-position-y: calc(100% + +35px) !important;
+      background-position-x: calc(100% + 10px) !important;
+    }
+    #nav-drawer {
+       background-image: url(https://new-edu.saby.ru/tensor/new_year/2026/autumn_theme/Innokentiy_tea.png) !important;
+       background-repeat: no-repeat !important;
+       background-position-y: calc(100% + 35px) !important;
+       background-position-x: calc(100% + 10px) !important;
+      }
+  `
+
+  document.head.append(style);
+})(
+  // @ts-ignore
+  unsafeWindow
+)
